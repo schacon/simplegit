a11bef06a3f659402fe7563abf99ad00de2209e6
Scott Chacon <schacon@gmail.com>
2008-03-15 10:31:28 -0700

    first commit

---
+++ lib/simplegit.rb
@@ -0,0 +1,25 @@
+# a super simple example class to use git in ruby
+class SimpleGit
+  
+  def initialize(git_dir = '.')
+    @git_dir = File.expand_path(git_dir)
+  end
+  
+  def show(treeish = 'master')
+    command("git show #{treeish}")
+  end
+
+  private
+  
+    def command(git_cmd)
+      Dir.chdir(@git_dir) do
+        return `#{git_cmd} 2>&1`.chomp
+      end
+    end
+  
+end
+
+if $0 == __FILE__
+  git = SimpleGit.new
+  puts git.show
+end
